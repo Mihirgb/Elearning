d1ac382d174dbbaf3f37cd693afd99f603c115c1
Mihir Gajbhiye <mihir.gajbhiye20@vit.edu>
2022-12-13 12:58:09 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,129 @@
+<?php
+  include('./dbConnection.php');
+  // Header Include from mainInclude 
+  include('./mainInclude/header.php'); 
+?>  
+    <!-- Start Video Background-->
+    <div class="container-fluid remove-vid-marg">
+      <div class="vid-parent">
+        <video playsinline autoplay muted loop>
+          <source src="video/banvid.mp4" />
+        </video>
+        <div class="vid-overlay"></div>
+      </div>
+      <div class="vid-content" >
+        <h1 class="my-content">Welcome to interview portal </h1>
+        <small class="my-content">Career Guidance for Schools students, College students & Professionals</small><br />
+        <?php    
+              if(!isset($_SESSION['is_login'])){
+                echo '<a class="btn btn-danger mt-3" href="#" data-toggle="modal" data-target="#stuRegModalCenter">Get Started</a>';
+              } else {
+                echo '<a class="btn btn-primary mt-3" href="student/studentProfile.php">My Profile</a>';
+              }
+          ?> 
+       
+      </div>
+    </div> <!-- End Video Background -->
+
+    <div class="container-fluid bg-danger txt-banner"> <!-- Start Text Banner -->
+        <div class="row bottom-banner">
+          <div class="col-sm">
+            <h5> <i class="fas fa-book-open mr-3"></i> 100+ Online Courses</h5>
+          </div>
+          <div class="col-sm">
+            <h5><i class="fas fa-users mr-3"></i> Expert Instructors</h5>
+          </div>
+          <div class="col-sm">
+            <h5><i class="fas fa-keyboard mr-3"></i> Lifetime Access</h5>
+          </div>
+          <div class="col-sm">
+            <h5><i class="fas fa-rupee-sign mr-3"></i> Money Back Guarantee*</h5>
+          </div>
+        </div>
+    </div> <!-- End Text Banner -->
+    
+      <!-- End Most Popular Course -->
+
+    <?php 
+    // Contact Us
+    include('./contact.php'); 
+    ?>  
+
+     <!-- Start Students Testimonial -->
+      <div class="container-fluid mt-5" style="background-color: #4B7289" id="Feedback">
+        <h1 class="text-center testyheading p-4"> Student's Review's </h1>
+        <div class="row">
+          <div class="col-md-12">
+            <div id="testimonial-slider" class="owl-carousel">
+            <?php 
+              $sql = "SELECT s.stu_name, s.stu_occ, s.stu_img, f.f_content FROM student AS s JOIN feedback AS f ON s.stu_id = f.stu_id";
+              $result = $conn->query($sql);
+              if($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()){
+                  $s_img = $row['stu_img'];
+                  $n_img = str_replace('../','',$s_img)
+            ?>
+              <div class="testimonial">
+                <p class="description">
+                <?php echo $row['f_content'];?>  
+                </p>
+                <div class="pic">
+                  <img src="<?php echo $n_img; ?>" alt=""/>
+                </div>
+                <div class="testimonial-prof">
+                  <h4><?php echo $row['stu_name']; ?></h4>
+                  <small><?php echo $row['stu_occ']; ?></small>
+                </div>
+              </div>
+              <?php }} ?>
+            </div>
+          </div>
+        </div>
+    </div>  <!-- End Students Testimonial -->
+
+    <div class="container-fluid bg-danger"> <!-- Start Social Follow -->
+        <div class="row text-white text-center p-1">
+          <div class="col-sm">
+            <a class="text-white social-hover" href="#"><i class="fab fa-facebook-f"></i> Facebook</a>
+          </div>
+          <div class="col-sm">
+            <a class="text-white social-hover" href="#"><i class="fab fa-twitter"></i> Twitter</a>
+          </div>
+          <div class="col-sm">
+            <a class="text-white social-hover" href="#"><i class="fab fa-whatsapp"></i> WhatsApp</a>
+          </div>
+          <div class="col-sm">
+            <a class="text-white social-hover" href="#"><i class="fab fa-instagram"></i> Instagram</a>
+          </div>
+        </div>
+    </div> <!-- End Social Follow -->
+
+    <!-- Start About Section -->
+    <div class="container-fluid p-4" style="background-color:#E9ECEF">
+      <div class="container" style="background-color:#E9ECEF">
+        <div class="row text-center">
+          <div class="col-sm">
+            <h5>About Us</h5>
+              <p>interview portal provides universal access to the world’s best education, partnering with top universities and organizations to offer video interview </p>
+          </div>
+          <div class="col-sm">
+            <h5>Category</h5>
+            <a class="text-dark" href="#">Video Interview</a><br />
+            <a class="text-dark" href="#">Senior Review</a><br />
+            <a class="text-dark" href="#">Resume Builder</a><br />
+            <a class="text-dark" href="#">Companywise Question</a><br />
+            <a class="text-dark" href="#"></a><br />
+          </div>
+          <div class="col-sm">
+            <h5>Contact Us</h5>
+            <p>interview portal<br> Near VIT college<br> upper depo, maharashtra <br> Mb.8505074337 </p>
+          </div>
+        </div>
+      </div>
+    </div> <!-- End About Section -->
+
+  <?php 
+    // Footer Include from mainInclude 
+    include('./mainInclude/footer.php'); 
+    
+  ?>  
